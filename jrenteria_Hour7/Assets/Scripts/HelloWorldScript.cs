b5028918de271868a7c03e9054060ce0c1fb19ce
JonathaJRenteria <jr1123030@swccd.edu>
2023-09-23 14:07:57 -0700

    tiys7 done

---
+++ jrenteria_Hour7/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello World");
+    }
+    void Update()
+    {
+    }
+}
---
+++ jrenteria_Hour7/Assets/Scripts/ImportantFunctions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class ImportantFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print ("Start runs before an object Updates"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print ("This is called once a frame");
+    }
+}
