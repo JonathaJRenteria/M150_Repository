1197b835e96df7b4da95a476a18b49007212294e
JonathaJRenteria <jr1123030@swccd.edu>
2023-10-28 17:42:20 -0700

    10 aligned

--- jrenteria_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ jrenteria_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -3,6 +3,21 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+
+    public float spacing = 2.0f;
+
+    void Start()
+    {
+        Vector3 referencePosition = new Vector3(-1.0f, 0.0f, -5.0f);
+       
+        Vector3 spawnPosition = referencePosition;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+            spawnPosition.x += spacing;
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
