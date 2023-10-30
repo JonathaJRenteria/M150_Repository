7493b95c34fde43cbb37d38e3c5dd21b4d72f992
JonathaJRenteria <jr1123030@swccd.edu>
2023-10-30 01:03:48 -0700

    I did the tutorial

---
+++ jrenteria_Hour11/Assets/Scripts/prefabCreator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabCreator : MonoBehaviour
+{
+    public GameObject Stickie;
+    public Vector3 spawnValues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    for (int i = 1; i <= 5; i++)
+    {
+            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
+            GameObject gameobject = Instantiate<GameObject>(Stickie, spawnposition, Quaternion.identity);
+    }
+    
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
